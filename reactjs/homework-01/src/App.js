129fd05bcadeedb8de289fdf70c2edb275c28a73
yekoo <extisque@yandex.ru>
2021-07-31 23:04:36 +0300

    Update App.js

--- reactjs/homework-01/src/App.js
+++ reactjs/homework-01/src/App.js
@@ -1,1 +1,20 @@
+import Message from './Message';
+import './App.css';
 
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Message message={props.message}/>
+      </header>
+      
+
+    </div>
+  );
+}
+
+export default App;
